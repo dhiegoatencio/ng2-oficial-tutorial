a371529d657e78606e34d643191979dd3095ab62
dhiegoatencio <dhiegoatencio@users.noreply.github.com>
2016-09-17 16:51:26 -0300

    created shared folder

--- app/app.module.ts
+++ app/app.module.ts
@@ -1,11 +1,11 @@
-import './rxjs-extensions';
+import './shared/rxjs-extensions';
 import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule }   from '@angular/forms';
 import { HttpModule }    from '@angular/http';
 // Imports for loading & configuring the in-memory web api
 import { InMemoryWebApiModule } from 'angular2-in-memory-web-api';
-import { InMemoryDataService }  from './in-memory-data.service';
+import { InMemoryDataService }  from './shared/in-memory-data.service';
 
 import { AppComponent }         from './app.component';
 import { DashboardComponent }   from './dashboard/dashboard.component';
