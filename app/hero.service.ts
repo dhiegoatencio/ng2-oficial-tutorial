56fe293e7c6a279203e38803c9b90df0840d8bf9
dhiegoatencio <dhiegoatencio@users.noreply.github.com>
2016-09-16 23:42:39 -0300

    Initial commit.

---
+++ app/hero.service.ts
@@ -0,0 +1,73 @@
+import {Injectable} from '@angular/core';
+import {Headers, Http} from '@angular/http';
+
+import 'rxjs/add/operator/toPromise';
+
+import {Hero} from './hero';
+
+@Injectable()
+export class HeroService {
+
+    private heroesUrl = 'app/heroes'; // url web api
+    private headers = new Headers({'Content-Type': 'application/json'});
+
+    constructor(private http: Http) {}
+
+    private handleError(error: any): Promise<any> {
+        console.error('HeroService. An error ocurred: ' + error);
+        return Promise.reject(error.message || error);
+    }
+
+    getHero(id: number): Promise<Hero> {
+        return this.getHeroes()
+            .then(heroes => heroes.find(
+                hero => hero.id === id
+            ));
+    }
+
+    delete(id: number): Promise<void> {
+        let url = `${this.heroesUrl}/${id}`;
+        return this.http.delete(url, {headers: this.headers})
+            .toPromise().then(() => null).catch(this.handleError);
+    }
+
+    create(name: string): Promise<Hero> {
+        return this.http
+            .post(
+                this.heroesUrl,
+                JSON.stringify({name: name}),
+                {headers: this.headers}
+            ).toPromise()
+            .then(res => res.json().data)
+            .catch(this.handleError);
+    }
+
+    update(hero: Hero): Promise<Hero> {
+        const url = `${this.heroesUrl}/${hero.id}`;
+        return this.http
+            .put(
+                url,
+                JSON.stringify(hero),
+                {headers: this.headers}
+            )
+            .toPromise()
+            .then(() => hero)
+            .catch(this.handleError);
+    }
+
+    getHeroes(): Promise<Hero[]> {
+        return this.http.get(this.heroesUrl)
+            .toPromise()
+            .then(response =>
+                response.json().data as Hero[]
+            ).catch(this.handleError);
+    }
+
+    getHeroesSlowly(): Promise<Hero[]> {
+        return new Promise<Hero[]>(
+            resolve => setTimeout(resolve, 2000)
+        ).then(() =>
+            this.getHeroes()
+        );
+    }
+}
